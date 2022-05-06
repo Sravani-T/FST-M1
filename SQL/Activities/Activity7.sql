7810b41d57b1f7f7ec857e3ac27d556576044f52
Sravani-T <104971776+Sravani-T@users.noreply.github.com>
2022-05-06 12:34:46 +0530

    Add files via upload

---
+++ SQL/Activities/Activity7.sql
@@ -0,0 +1,14 @@
+-- SQL statement to find the total purchase amount of all orders.
+select SUM(purchase_amount) AS "Total sum" from orders;
+
+-- SQL statement to find the average purchase amount of all orders.
+select AVG(purchase_amount) AS "Average" from orders;
+
+-- SQL statement to get the maximum purchase amount of all the orders.
+select MAX(purchase_amount) AS "Maximum" from orders;
+
+-- SQL statement to get the minimum purchase amount of all the orders.
+select MIN(purchase_amount) AS "Minumum" from orders;
+
+-- SQL statement to find the number of salesmen listed in the table.
+select COUNT(distinct salesman_id) AS "Total count" from orders;
